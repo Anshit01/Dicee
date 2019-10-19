91703f540e85e7b0d6eb20efada13ab8d9d2d800
Anshit <bhardwaj.anshit1379@gmail.com>
2019-10-19 12:57:19 +0530

    final commit

--- app/src/main/java/com/anshit/dicee/MainActivity.java
+++ app/src/main/java/com/anshit/dicee/MainActivity.java
@@ -2,7 +2,14 @@ package com.anshit.dicee;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final int[] diceArray = {R.drawable.dice1,
+                R.drawable.dice2,
+                R.drawable.dice3,
+                R.drawable.dice4,
+                R.drawable.dice5,
+                R.drawable.dice6};
+
+        Button rollbtn = findViewById(R.id.rollbtn);
+        final ImageView dice_left = findViewById(R.id.dice_left);
+        final ImageView dice_right = findViewById(R.id.dice_right);
+        rollbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random rand = new Random();
+                int r = rand.nextInt(6);
+                dice_left.setImageResource(diceArray[r]);
+                r = rand.nextInt(6);
+                dice_right.setImageResource((diceArray[r]));
+            }
+        });
+
     }
 }
